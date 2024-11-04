edcab9096ebc89ce9472d742b2becc52ba97e0f7
AndrewKooklin <bolt999@yandex.ru>
2024-11-04 09:32:41 +0300

    Add RandomPhraseModel

---
+++ MyFlatWPF/Model/RandomPhraseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFlatWPF.Model
+{
+    public class RandomPhraseModel : BaseModel
+    {
+        public string Phrase { get; set; }
+
+        [NotMapped]
+        public string InputChangePhraseError { get; set; }
+    }
+}
