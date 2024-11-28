11913120b05f93f2d6fd13e21d42983ed31c2bcf
AndrewKooklin <bolt999@yandex.ru>
2024-11-28 09:42:20 +0300

    Add Cb_SelectionChanged method

--- MyFlatWPF/View/ManagementView/AllOrdersView.xaml.cs
+++ MyFlatWPF/View/ManagementView/AllOrdersView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MyFlatWPF.Data.Repositories.API;
+using MyFlatWPF.Model;
+using MyFlatWPF.Model.ManagementModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,34 @@ namespace MyFlatWPF.View.ManagementView
     /// </summary>
     public partial class AllOrdersView : UserControl
     {
+        APIManagementRepository _api = new APIManagementRepository();
+
         public AllOrdersView()
         {
             InitializeComponent();
+        }
+
+        private async void Cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox)
+            {
+                var comboBox = sender as ComboBox;
+                if (comboBox.SelectedItem == comboBox.SelectionBoxItem)
+                {
+                    return;
+                }
+
+                if (this.dgOrders.SelectedItem != null && comboBox.SelectedItem != null)
+                {
+                    OrderModel om = (OrderModel)this.dgOrders.SelectedItem;
+                    var selectedStatus = comboBox.SelectedItem;
+                    ChangeStatusModel csm = new ChangeStatusModel();
+                    csm.Id = om.Id;
+                    csm.Status = selectedStatus.ToString();
+                    bool result = await _api.ChangeStatusOrder(csm);
+                    return;
+                }
+            }
         }
     }
 }
