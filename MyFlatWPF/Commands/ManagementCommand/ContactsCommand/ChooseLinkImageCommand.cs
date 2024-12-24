1acbd0d9269fa3518b37a58f922ecd493c9afe45
AndrewKooklin <bolt999@yandex.ru>
2024-12-24 15:36:39 +0300

    Add ChooseLinkImageCommand

---
+++ MyFlatWPF/Commands/ManagementCommand/ContactsCommand/ChooseLinkImageCommand.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using MyFlatWPF.HelpMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MyFlatWPF.Commands.ManagementCommand.ContactsCommand
+{
+    public class ChooseLinkImageCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private ImageConverter _ic = new ImageConverter();
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.DefaultExt = ".jpg";
+            dialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|" +
+                            "PNG Files (*.png)|*.png|" +
+                            "JPG Files (*.jpg)|*.jpg|" +
+                            "GIF Files (*.gif)|*.gif|" +
+                            "WEBP Files (*.webp)|*.webp";
+            dialog.Multiselect = false;
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                string filePath = dialog.FileName;
+                byte[] image = _ic.ImageToByteArray(filePath);
+                StaticImage.NewSocialLinkImage = image;
+                if (parameter is TextBlock)
+                {
+                    TextBlock tbImageName = (TextBlock)parameter;
+                    tbImageName.Text = $"Choosed : \"{dialog.SafeFileName}\"";
+                    App.LinkEditView.iLinkImage.Source =
+                        _ic.ByteArrayToImage(StaticImage.NewSocialLinkImage);
+                }
+            }
+        }
+    }
+}
