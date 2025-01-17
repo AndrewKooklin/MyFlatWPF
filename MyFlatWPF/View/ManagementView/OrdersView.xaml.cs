69d3260fa076d0b36949b98d7f6053597d3e3179
AndrewKooklin <bolt999@yandex.ru>
2025-01-17 07:56:57 +0300

    Remove unusing using

--- MyFlatWPF/View/ManagementView/OrdersView.xaml.cs
+++ MyFlatWPF/View/ManagementView/OrdersView.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using MyFlatWPF.Data.Repositories.API;
 using MyFlatWPF.Model;
 using MyFlatWPF.Model.ManagementModel;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyFlatWPF.View.ManagementView
 {
